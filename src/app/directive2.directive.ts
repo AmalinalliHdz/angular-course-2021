a10d0f0fb171cec85f9c717eccdd821c58e7e400
AmalinalliHdz <145622919+AmalinalliHdz@users.noreply.github.com>
2024-01-15 17:04:24 -0600

    2 files modified

--- src/app/directive2.directive.ts
+++ src/app/directive2.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, Input, TemplateRef, ViewContainerRef, OnChanges } from '@angular/core';
+import {
+  Directive,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+  OnChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[ngIfAmalinalli]',
@@ -10,13 +16,22 @@ export class Directive2Directive implements OnChanges {
     private templateRef: TemplateRef<any>,
     private viewContainerRef: ViewContainerRef
   ) {
-    if (this.ngIfAmalinalli) {
+    /* if (this.ngIfAmalinalli) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }*/
+  }
+  ngOnChanges(changes: any) {
+    console.log('changes: ', changes.ngIfAmalinalli.currentValue);
+    this.setViewContainer(changes.ngIfAmalinalli.currentValue);
+  }
+
+  setViewContainer(show: boolean) {
+    if (show) {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainerRef.clear();
     }
-  }
-  ngOnChanges(changes: any){
-    console.log("changes: ", changes)
   }
 }
