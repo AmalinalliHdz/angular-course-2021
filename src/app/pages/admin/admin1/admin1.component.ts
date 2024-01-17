68eff5659d56113b7885131be27b6a9ea532d807
AmalinalliHdz <145622919+AmalinalliHdz@users.noreply.github.com>
2024-01-17 11:12:30 -0600

    1 file modified

--- src/app/pages/admin/admin1/admin1.component.ts
+++ src/app/pages/admin/admin1/admin1.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, concat } from 'rxjs';
 
 @Component({
   selector: 'app-admin1',
   templateUrl: './admin1.component.html',
-  styleUrls: ['./admin1.component.css']
+  styleUrls: ['./admin1.component.css'],
 })
 export class Admin1Component implements OnInit {
+  name = 'omar';
+  auxJson = { name: 'Ale', lastName: 'Sanchez' };
+  auxObs$ = new BehaviorSubject<any>([1, 2, 3, 4, 5]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onAdd() {
+    const aux = this.auxObs$.getValue();
+    const aux1 = aux.concat([6, 7]);
+    this.auxObs$.next(aux1);
   }
-
 }
