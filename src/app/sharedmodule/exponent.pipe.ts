16cf97ec93390dd0229610448e07ee623d375583
AmalinalliHdz <145622919+AmalinalliHdz@users.noreply.github.com>
2024-01-18 16:29:44 -0600

    2 files modified

--- src/app/sharedmodule/exponent.pipe.ts
+++ src/app/sharedmodule/exponent.pipe.ts
@@ -4,8 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'exponent',
 })
 export class ExponentPipe implements PipeTransform {
-  
-  transform(value: number): number {
-    return value * 2;
+  transform(value: number, exponent: number): number {
+    return Math.pow(value, exponent);
   }
 }
